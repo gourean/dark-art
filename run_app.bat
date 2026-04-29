69864f00ca0bdc9579ce44b4abb9e10ef16f3b22
gourean <gourean@gmail.com>
2026-04-29 17:28:58 +0800

    Update for rlang 1.2.0, renv support, and v1 legacy hardening

--- run_app.bat
+++ run_app.bat
@@ -5,14 +5,8 @@ echo           SUMMONING DARK-ART.....
 echo ----------------------------------------
 
 
-:: Try to use the Rscript found in the standard location
-SET "RSCRIPT=C:\Program Files\R\R-4.4.3\bin\Rscript.exe"
-
-IF NOT EXIST "%RSCRIPT%" (
-    ECHO Rscript not found at default location.
-    ECHO Trying global 'Rscript' command...
-    SET "RSCRIPT=Rscript"
-)
+:: Use the system Rscript (ensures compatibility with 'rig' or manual updates)
+SET "RSCRIPT=Rscript"
 
 "%RSCRIPT%" -e "shiny::runApp('app.R', launch.browser=TRUE)"
 
